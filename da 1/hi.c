999cf7dc63c3293f476c94b849870c116e6b7cb4
aadya1304 <aadyaagarwal793@gmail.com>
2026-08-14 11:14:48 +0530

    Update hi.c

---
+++ da 1/hi.c
@@ -0,0 +1,12 @@
+//Write a program to input two numbers and display thei
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d", a);
+    scanf("%d", b);
+    int sum = a+b;
+    printf("sum= %d", sum);
+    return 0;
+}
